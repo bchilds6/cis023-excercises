d6c93a676ef6fed38a826836daeb7c1f7ca3fe8f
Bradley Childs <bchilds1@students.solano.edu>
2019-10-23 18:50:50 -0700

    Added comments

--- excercise02/excercise02/transportation.cpp
+++ excercise02/excercise02/transportation.cpp
@@ -1,11 +1,14 @@
 #include "transportation.h"
 
 
-
+// default constructor
 transportation::transportation() {
 
 }
+
+// constructor with values
 transportation::transportation(int iNumberOfPassengers, string sDestinationName, double dDistance) :
+	// initialize the class variables
 	numberOfPassengers(iNumberOfPassengers),
 	destinationName(sDestinationName),
 	milesToDestination(dDistance)
@@ -13,31 +16,42 @@ transportation::transportation(int iNumberOfPassengers, string sDestinationName,
 	
 }
 
-
+// destructor
 transportation::~transportation() {
 
 }
 
+// getters
+
+// number of passengers
 int transportation::getNumberOfPassengers() {
-	return numberOfPassengers;
+	return numberOfPassengers;		
 }
 
-void transportation::setNumberOfPassengers(int iInput) {
-	numberOfPassengers = iInput;
-}
-
-string transportation::getDestinationName() {
-	return destinationName;
-}
-
-void transportation::setDestinationName(string sInput) {
-	destinationName = sInput;
-}
-
+// distance to destination
 double transportation::getMilesToDestination() {
 	return milesToDestination;
 }
 
+// destination name
+string transportation::getDestinationName() {
+	return destinationName;
+}
+
+
+// setters
+
+// destination name
+void transportation::setDestinationName(string sInput) {
+	destinationName = sInput;
+}
+
+// number of passengers
+void transportation::setNumberOfPassengers(int iInput) {
+	numberOfPassengers = iInput;
+}
+
+// distance to destination
 void transportation::setMilesToDestination(double dInput) {
 	milesToDestination = dInput;
 }
